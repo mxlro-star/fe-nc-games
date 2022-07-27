f2894a4a440bc37690fbb3bf29298d68b4625b4e
Mohamed Elrofai <m.elrofai@icloud.com>
2022-07-27 11:44:53 +0100

    Revert "add ReviewCard component and routes"

--- src/App.js
+++ src/App.js
@@ -1,27 +1,23 @@
-import "./App.css";
-
-import Header from "./components/Header";
-
-import Home from "./components/Home";
-import Categories from "./components/Categories";
-import Navigation from "./components/Navigation";
-import AllReviews from "./components/AllReviews";
-
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Header />
-        <Navigation />
-
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/reviews" element={<AllReviews />} />
-          <Route path="/categories" element={<Categories />} />
-        </Routes>
-      </BrowserRouter>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
--- src/api.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const api = {};
-export default api;
-
-api.getReviews = axios({
-  method: "get",
-  url: "http://localhost:9090/api/reviews",
-});
